ca28a392620423692e71dce0cfd94a001297d3a2
mohsinraz3 <mohsinraz3@gmail.com>
2023-12-09 23:14:09 +0500

    error-file handling|classes

--- class4.py
+++ class4.py
@@ -1,5 +1,11 @@
-from typing import Any  
+""" from typing import Any  
 
 fruits: list[str] = ["saib", "angoor", "anaar","kharboza"]
 print(f'my favourite phaal is {fruits[3]}')
+"""
+from typing import Tuple,Dict
 
+def my_function(a:int,b:int, *abc:int, **xyz:int):
+    print(f"1st param: {a},\n2nd param: {b}, \n3rd Argumenets {abc}, \n4th All positional args  {xyz}")
+    
+my_function(1,3,4,5,6,7,8, d=3,f=5)
